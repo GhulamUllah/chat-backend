a2921d2ed6ef0169443881c14e763a5bec878360
Ghulam Ullah <ghulamullahkhan84@gmail.com>
2023-10-15 12:41:15 +0500

    here is a backend

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
+const cors = require('cors')
+
+
+
+const app = express()
+const users = [{}]
+
+
+app.use(cors())
+
+app.get('/', (req, res) => res.send("Welcome To Chatbot Backend"))
+
+
+const port = 5000 || process.env.PORT
+
+
+const server = http.createServer(app)
+
+
+const io = socketIO(server)
+
+io.on('connection', (socket) => {
+    console.log("connected")
+    socket.on('joined', ({ user }) => {
+        users[socket.id] = user
+        socket.emit('welcome', { user: users[socket.id], message: "Welcome to Chatbot",id:socket.id })
+        socket.broadcast.emit('userjoin', { user: 'Admin', message: users[socket.id] + " has joined" })
+        socket.on('disconnect',()=>{
+            socket.broadcast.emit('leave',{user:"Admin" ,message:users[socket.id]+ ' has left chat'})
+        })
+    })
+    
+    socket.on('sendmessage',({id,messagetosent})=>{
+        io.emit('recieved',{user:users[id],message:messagetosent,id})
+    })
+
+
+})
+
+server.listen(port, () => console.log(`App is running on http://localhost:${port}`))
+
